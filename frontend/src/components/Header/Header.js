36710143565db6f425bab6233486a74b2880c8b8
abhishekpawar1060 <abhishekpawar7218@gmail.com>
2024-07-08 18:25:32 +0530

    Create Header of the project

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import classes from './header.module.css'
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+
+    const user = {
+       name: 'Ram',
+
+    };
+
+    const cart = {
+        totalCount : 10,
+    };
+
+
+    const logout = () => {}
+
+    return (
+        <header className={classes.header}>
+            <div className={classes.container}>
+                <Link to="/" className={classes.logo}>MyFood</Link>
+                <nav>
+                    <ul>
+                        {
+                            user? (
+                            <li className={classes.menu_container}>
+                                <Link to="/profile">{user.name}</Link>
+                                <div className={classes.menu}>
+                                    <Link to="/profile">Profile</Link>
+                                    <Link to="/orders">Orders</Link>
+                                    <a onClick={logout}>LogOut</a>
+                                </div>
+                            </li> 
+                            ) : (
+                                <Link to="/login">Login</Link>
+                            ) 
+                        }
+
+                        <li>
+                            <Link to="/cart">
+                                Cart
+                                {cart.totalCount > 0 && <span className={classes.cart_count}>{cart.totalCount}</span>}
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div> 
+        </header>
+    )
+}
+
