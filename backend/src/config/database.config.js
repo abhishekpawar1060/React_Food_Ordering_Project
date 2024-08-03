63f7e26b51204dccfb91927a5f721c74a4aa4e78
abhishekpawar1060 <abhishekpawar7218@gmail.com>
2024-08-03 19:22:47 +0530

    add url

--- backend/src/config/database.config.js
+++ backend/src/config/database.config.js
@@ -11,8 +11,8 @@ set('strictQuery', true)
 export const dbconnect = async () => {
     try {
         connect(process.env.MONGO_URI, {
-            // useNewUrlParser: true,
-            // useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
         });
         
         await seedUsers();
